cee4adbfc906606ff2fe188980d3ff950e3923c3
flourtowel <jon.bjerke@pm.me>
2019-03-12 21:14:59 -0400

    Added test objects

---
+++ script.py
@@ -0,0 +1,13 @@
+#script.py
+#
+#Author: flourtowel
+#Date:   12 Mar 2019
+#
+#This is the project The Boredless Tourist from codecademy PRO
+#
+
+#List of destinations
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+
+#List for test traveler
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
