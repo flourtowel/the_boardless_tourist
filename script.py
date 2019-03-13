9f89968e0c09c3f7c0a265ff9d2dc0eba5bd79ed
flourtowel <jon.bjerke@pm.me>
2019-03-12 21:29:10 -0400

    Added def get_destination_index

--- script.py
+++ script.py
@@ -11,3 +11,11 @@ destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Pa
 
 #List for test traveler
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+
+def 
+
+print(get_destination_index('Pris, France'))
